aeda05996cd4b8f5e927d717a846f810468e9166
Artem Ryndin <artemryndinjob@gmail.com>
2021-11-01 23:05:07 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 350;
+        int bonusAmount = 20;
+
+        int miles = price/bonusAmount;
+        System.out.println("Бонусных миль: " + miles);
+        
+    }
+}
